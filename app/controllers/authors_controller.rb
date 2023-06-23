c8733e8fe85404357d70178dd7018e666a38352a
Wangari Samantha <samanthanyams5@gmail.com>
2023-06-23 06:37:09 +0300

    complete

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,6 @@
 class AuthorsController < ApplicationController
-  
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
   def show
     author = Author.find(params[:id])
 
@@ -7,12 +8,20 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    author = Author.create(author_params)
-
+    author = Author.create!(author_params)
     render json: author, status: :created
   end
 
+
   private
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  end
+
+  def render_not_found_response
+    render json: { error: "Author not found" }, status: :not_found
+  end
   
   def author_params
     params.permit(:email, :name)
